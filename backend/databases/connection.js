ed5b2b8c4448416b67b404278dc1430ba9b39178
Arpit Mishra <arpitmishra5096@gmail.com>
2026-09-01 02:51:56 +0530

    db connection

--- backend/databases/connection.js
+++ backend/databases/connection.js
@@ -4,6 +4,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 }).then(() => {
     console.log('connection eshtablished');
+    console.log("Connected DB:", mongoose.connection.name);
 }).catch((e) => {
     console.log(e);
     console.log('connection not eshtablish');
